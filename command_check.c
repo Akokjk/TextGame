65994bc721f331142716becb0379b366ac38fa89
Aaron Barack <abarack@ucsc.edu>
2018-04-29 17:11:51 -0700

    Fixed some errors

--- command_check.c
+++ command_check.c
@@ -1,38 +1,44 @@
 #include "prototypes.h"
 #define NUM_OF_COMMANDS 1 
 
-#define COMMAND(x) \
-char* result; \ 
-sprintf (result, "command%d", x); \
-return &(#result) \ 
 
 	
-//to avoid unnessary function prototpye creating marcos the commmand_check function is at the botttom 
+void help_com(char* input){ //help command
+	printf("You have accesssed the help function.\n Hizzah!\n");
+}
 
-void command0(char* input){ //help command 
-	printf("You have accesssed the help function.\n"); 
+/**********************************************************************************************  
+* Everytime a new command is created the address of the command function needs to be put into 
+*  the get_command array, the NUM_OF_COMMANDS needs to be updated, and the command_checker 
+* commands array needs to be updated. I wish I could simplify the process, but it's the best 
+* I can implement right now.  
+**********************************************************************************************/
+int get_command(int value)
+{
+    int commands[NUM_OF_COMMANDS] = {&help_com};
+    return commands[value];
+
 }
 
 void command_check(char* input)
 {
-	//This will check to see if the input matches any possible command 
-	char* commands[NUM_OF_COMMANDS] = {"help"};   
-	int i; 
-	int check = -1; 
+	//This will check to see if the input matches any possible command
+	char* commands[NUM_OF_COMMANDS] = {"help"};
+	int i;
+	int check = -1;
 	for (i = 0; i < NUM_OF_COMMANDS; i++){
-		if (strcmp(commands[i], input) == 0){
-			check = i; 
-			break; 
+		if (strcmp(commands[i], strtok(input, " ")) == 0){ //looks at frist word in the input 
+			check = i;
+			break;
 		}
 	}
+	//We only really care when check is equal to zero 
 	if (check <= -1){
-		printf("Unknown Command.\n"); 
+		printf("Unknown Command.\n");
 	}
 	else {
-		//This is probably one of the funnest things in c 
-		void (*command_ptr)(char*) = COMMAND(check); //generates a function pointer address using the macro 
-		(*command_ptr)(input); //Runs the function with the user input 
+		void (*command_ptr)(char*) = get_command(check); //generates a function pointer address using array found in get_command 
+		(*command_ptr)(input); //Runs the function with the user input
 	}
-	
-}
 
+}
